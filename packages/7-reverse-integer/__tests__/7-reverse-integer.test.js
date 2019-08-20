0fa05ae779725a5b2547508e28ae0d318d82ae2a
xiaosong <kunkkaco@gmail.com>
2019-08-20 11:35:24 +0800

    feat: 完成整数反转

---
+++ packages/7-reverse-integer/__tests__/7-reverse-integer.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const reverse = require('..');
+
+describe('7-reverse-integer', () => {
+  it('123', () => {
+    expect(reverse(123)).toBe(321);
+  });
+
+  it('负数', () => {
+    expect(reverse(-123)).toBe(-321);
+  });
+
+  it('首位为零', () => {
+    expect(reverse(120)).toBe(21);
+  });
+
+  it('超出范围', () => {
+    expect(reverse(2147483647)).toBe(0);
+  });
+
+  it('负数超出范围', () => {
+    expect(reverse(-2147483648)).toBe(0);
+  });
+});
---
+++ packages/7-reverse-integer/lib/v1.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = reverse;
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+function reverse(x) {
+  const flag = 0 <= x ? 1 : -1;
+  const str = reverseString((x * flag).toString());
+  const r = parseInt(str, 10) * flag;
+  const max = Math.pow(2, 31);
+  return r < -1 * max || r > max - 1 ? 0 : r;
+}
+
+// 反转一个字符串:
+// https://www.html.cn/30-seconds-of-code/#reversestring
+const reverseString = str => [...str].reverse().join('');
+
+const reverseStr = str => {
+  let s = '';
+  for (let i = str.length - 1; 0 <= i; i--) {
+    s += str[i];
+  }
+  return s;
+};
